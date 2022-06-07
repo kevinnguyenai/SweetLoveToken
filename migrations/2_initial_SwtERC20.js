a7e0e8220383b5fa806d08ca36390ac374f19756
kevinnguyenai <kevin.nguyen.ai@gmail.com>
2022-06-07 22:17:32 +0700

    dev migrate

--- migrations/2_initial_SwtERC20.js
+++ migrations/2_initial_SwtERC20.js
@@ -3,6 +3,13 @@ var SWLERC20 = artifacts.require("./SWLToken.sol");
 module.exports = async function(deployer, network, accounts) {
     if (network == "development") {
         await deployer.deploy(SWLERC20);
+    }
+    else if (network == "rinkeby") {
+        await deployer.deploy(SWLERC20);
+    } else if (network == "kovan") {
+        await deployer.deploy(SWLERC20);
+    } else if (network == "serenity") {
+        await deployer.deploy(SWLERC20);
     } else {
         // used in future when deploy Rinkeby or Production mainnet
     }
--- truffle.js
+++ truffle.js
@@ -1,10 +1,99 @@
+const { 
+    projectId,
+    alchemyKey,
+    aura_rpc,
+    aura_network_id,
+    mnemonicPharse,
+    mnemonicKeplr,
+} = require('./.secret.json');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
 module.exports = {
     networks: {
         development: {
             host: "127.0.0.1",
             port: 7545,
             network_id: "*" // Match any network id
-        }
+        },
+        // Useful for deploying to a public network.
+        // NB: It's important to wrap the provider as a function.
+        ropsten: {
+            provider: () => new HDWalletProvider({
+                mnemonic: {
+                    phrase: mnemonicPharse
+                },
+                providerOrUrl: `https://ropsten.infura.io/v3/${projectId}`,
+                numberOfAddresses: 2,
+                shareNonce: true,
+                derivationPath: "m/44'/1'/0'/0/"
+            }),
+            network_id: 3, // Ropsten's id
+            gas: 5500000, // Ropsten has a lower block limit than mainnet
+            confirmations: 2, // # of confs to wait between deployments. (default: 0)
+            timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+            skipDryRun: true // Skip dry run before migrations? (default: false for public nets )
+        },
+        rinkeby: {
+            provider: () => new HDWalletProvider({
+                mnemonic: {
+                    phrase: mnemonicPharse
+                },
+                providerOrUrl: `https://rinkeby.infura.io/v3/${projectId}`,
+                numberOfAddresses: 2,
+                shareNonce: true,
+                derivationPath: "m/44'/1'/0'/0/"
+            }),
+            network_id: 4, // Rinkeby's id
+            gas: 5500000, // Rinkeby has a lower block limit than mainnet
+            confirmations: 2, // # of confs to wait between deployments. (default: 0)
+            timeoutBlocks: 200, // # of blocks before a deployment times out (minimum/default:50)
+            skipDryRun: true, // Skip dry run before migrations ? ( default: false)
+            networkCheckTimeout: 999999
+        },
+        rinkebyAlt: {
+            provider: () => new HDWalletProvider({
+                mnemonic: {
+                    phrase: mnemonicPharse
+                },
+                providerOrUrl: `https://eth-rinkeby.alchemyapi.io/v2/${alchemyKey}`,
+                numberOfAddresses: 2,
+                shareNonce: true,
+                derivationPath: "m/44'/1'/0'/0/"
+            }),
+            network_id: 4, // Rinkeby's id
+            gas: 5500000, // Rinkeby has a lower block limit than mainnet
+            confirmations: 2, // # of confs to wait between deployments. (default: 0)
+            timeoutBlocks: 200, // # of blocks before a deployment times out (minimum/default:50)
+            skipDryRun: true, // Skip dry run before migrations ? ( default: false)
+            networkCheckTimeout: 999999
+        },
+        kovan: {
+            provider: () => new HDWalletProvider({
+                mnemonic: {
+                    phrase: mnemonicPharse
+                },
+                providerOrUrl: `https://kovan.infura.io/v3/${projectId}`,
+                numberOfAddresses: 2,
+                shareNonce: true,
+                derivationPath: "m/44'/1'/0'/0/"
+            }),
+            network_id: 42, // Kovan's id
+            gas: 5500000, // Kovan has a lower block limit than mainnet
+            confirmations: 2, // # of confs to wait between deployments. (default: 0)
+            timeoutBlocks: 200, // # of blocks before a deployment times out (minimum/default:50)
+            skipDryRun: true, // Skip dry run before migrations ? ( default: false)
+            networkCheckTimeout: 999999
+        },
+        serenity: {
+            provider: () => new HDWalletProvider({
+                mnemonic: {
+                    phrase: mnemonicKeplr 
+                },
+                providerOrUrl: `${aura_rpc}`
+            }),
+            network_id: `${aura_network_id}`,
+            chain_id: `${aura_network_id}`
+        },
     },
     mocha: {},
     compilers: {
